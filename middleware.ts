33452d0ceeffeb4b32cd2c45a08465d1d26ca661
hazem-the-dev <hazemgamaltaha3@gmail.com>
2025-08-02 19:15:44 +0300

    removing security from an API route :)

--- middleware.ts
+++ middleware.ts
@@ -8,7 +8,7 @@ const isPublicRoute = createRouteMatcher(
         '/api/members/get-all',
         '/api/gyms/full-data',
         '/api/gyms/activate-member',
-        '/api/branches/get-branches',
+        '/api/gyms/branches/get-branches',
     ]
 )
 
